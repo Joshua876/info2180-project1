f47db5333efdddc452029c9d81bce095992583d9
Joshua876 <joshua.mcintosh@mymona.uwi.edu>
2021-10-24 22:37:44 -0500

    Updated css and sprinkle of java

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 /* Add your JavaScript to this file */
+window.onload=function(){
+    var mess=document.getElementsByClassName("message")[0]; 
+    var sub=document.querySelector("button");
+    sub.addEventListener("click", function(event){
+        event.preventDefault();
+        var email=document.getElementById("email");
+        if(email.value == ""){
+            mess.innerHTML="Please enter a valid email address."
+        }
+        else{
+            mess.innerHTML="Thank you! Your email address has been added to our mailinglist!"
+        }
+    });
+}
